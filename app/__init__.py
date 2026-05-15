74af1c65166226c8659de7fd53248ec8abcfa114
tsquainoo <tsquainoo@yahoo.com>
2026-05-15 17:16:49 +0100

    Fix Render deployment and Flask app structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+
     app.config['SECRET_KEY'] = 'earthquake-secret'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///earthquake.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+        "DATABASE_URL",
+        "sqlite:////tmp/earthquake.db"
+    )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
@@ -15,7 +20,9 @@ def create_app():
     app.register_blueprint(main)
 
     with app.app_context():
-        from .models import Country, Region, Earthquake
-        db.create_all()
+        try:
+            db.create_all()
+        except Exception as e:
+            print("DB init error:", e)
 
     return app
