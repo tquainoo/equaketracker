875510b901ff00ac7671ce5d7c1a01fa7dd1af6f
tsquainoo <tsquainoo@yahoo.com>
2026-05-15 21:32:51 +0100

    update

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,17 @@ def create_app():
     
     database_url = "postgresql://earthquake_db_rhjn_user:1BCTa1hw4T5M0pqIZerbQDXUQsLwhAOf@dpg-d83klujeo5us73bcp9b0-a.ohio-postgres.render.com:5432/earthquake_db_rhjn"
 
+    # FIX 2: FORCE SSL (CRITICAL for Render)
+    if "postgresql://" in database_url:
+        database_url += "?sslmode=require"
+
+
+    # FIX 3: production-safe pool settings
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+        "pool_pre_ping": True,
+        "pool_recycle": 280
+    }
+
     app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
